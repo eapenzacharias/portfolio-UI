c0dffa2424386dbede00b5500002928aa3aa6e83
eapenzacharias <eapenzac@gmail.com>
2022-06-03 01:11:05 +0200

    Fixed navbar responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
   return (
     <nav className="navigation">
       <a href="/" className="brand-name">
-        <img src={require("../assets/images/logo_dark_498x112.png")} alt="viewmy.pro" height="100%" />
+        <img src={require("../assets/images/logo_dark_498x112.png")} alt="viewmy.pro" />
       </a>
       <button
         className="hamburger"
@@ -15,17 +15,8 @@ const NavBar = () => {
         }}
       >
         {/* icon from heroicons.com */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="white"
-        >
-          <path
-            fillRule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-            clipRule="evenodd"
-          />
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
         </svg>
       </button>
       <div
