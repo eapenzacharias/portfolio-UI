06fd016fbf9ae0137b0c8a63da3a51cc5b51e916
eapenzacharias <eapenzac@gmail.com>
2022-06-03 20:33:03 +0200

    Fixed nav bar animation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 const NavBar = () => {
   const [isNavExpanded, setIsNavExpanded] = useState(false)
   return (
-    <nav className="navigation">
+    <nav className="navigation drop-shadow-lg bg-surfaceL">
       <a href="/" className="brand-name">
         <img src={require("../assets/images/logo_dark_498x112.png")} alt="viewmy.pro" />
       </a>
@@ -21,18 +21,21 @@ const NavBar = () => {
       </button>
       <div
         className={
-          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+          isNavExpanded ? "navigation-menu expanded ease-in-out duration-1000" : "navigation-menu"
         }
       >
-        <ul>
+        <ul className="fadeIn">
           <li>
-            <a href="/home">Home</a>
+            <a href="#home">Home</a>
           </li>
           <li>
-            <a href="/about">About</a>
+            <a href="#about">About</a>
           </li>
           <li>
-            <a href="/contact">Contact</a>
+            <a href="#works">Works</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
           </li>
         </ul>
       </div>
