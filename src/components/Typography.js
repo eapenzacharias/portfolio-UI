855072555501099c6d1bd23a5d8b5b4308a94689
eapenzacharias <eapenzac@gmail.com>
2022-06-21 21:10:41 +0200

    Modularized typography function

--- src/components/Typography.js
+++ src/components/Typography.js
@@ -1,11 +1,19 @@
-export const MainHeading = ({ text }) => {
-  return (
-    <h1 className="text-6xl font-bold text-primaryD ">{text}</h1>
-  )
-}
+import PropTypes from 'prop-types';
 
-export const SubHeading = ({ text }) => {
-  return (
-    <h3 className="text-2xl font-bold text-textDark">{text}</h3>
-  )
-}
+const MyText = ({ text, type }) => {
+  switch (type) {
+    case 'h1':
+      return <h1 className="text-6xl font-bold text-primaryD ">{text}</h1>;
+    case 'h3':
+      return <h3 className="text-2xl font-bold text-textDark">{text}</h3>;
+    default:
+      return <h3 className="text-2xl font-bold text-textDark">{text}</h3>;
+  }
+};
+
+MyText.propTypes = {
+  text: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+};
+
+export default MyText;
