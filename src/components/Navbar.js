122c175b1e33dfd3473a5349b19f45b4e6d52c06
eapenzacharias <eapenzac@gmail.com>
2022-06-21 21:11:14 +0200

    Changed logo import

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,28 @@
-import "../assets/styles/Navbar.css";
-import { useState } from "react";
+import '../assets/styles/Navbar.css';
+import { useState } from 'react';
+import logo from '../assets/images/logo_dark_498x112.png';
 
 const NavBar = () => {
-  const [isNavExpanded, setIsNavExpanded] = useState(false)
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
   return (
     <nav className="navigation drop-shadow-lg bg-surfaceL">
       <a href="/" className="brand-name">
-        <img src={require("../assets/images/logo_dark_498x112.png")} alt="viewmy.pro" />
+        <img src={logo} alt="viewmy.pro" />
       </a>
       <button
         className="hamburger"
+        type="button"
         onClick={() => {
           setIsNavExpanded(!isNavExpanded);
         }}
       >
-        {/* icon from heroicons.com */}
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
           <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
         </svg>
       </button>
       <div
         className={
-          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+          isNavExpanded ? 'navigation-menu expanded' : 'navigation-menu'
         }
       >
         <ul className="fadeIn">
@@ -41,6 +42,6 @@ const NavBar = () => {
       </div>
     </nav>
   );
-}
+};
 
 export default NavBar;
